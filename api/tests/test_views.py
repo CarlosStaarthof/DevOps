67afc76a34f15086e8d80ec3d522c82baba54cfb
Carlos S <csalinas1029@gmail.com>
2025-07-08 21:16:46 +0100

    fix: miss a charachter/ fix the actual issue

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -3,23 +3,22 @@ from rest_framework import status
 from django.urls import reverse
 
 class BookViewTest(APITestCase):
-    
     def test_response_is_correct(self):
         url = reverse('api:books')
         response = self.client.get(url, format='json')  
         assert response.status_code == status.HTTP_200_OK
         body = response.json()
-        assert body ['hello'] == 'django'
+        assert body['hello'] == 'django'
         
 
 class HealthViewTest(APITestCase):
-
-        def test_response_is_correct(self):
+    def test_response_is_correct(self):
         url = reverse('api:health')
         response = self.client.get(url, format='json')  
         assert response.status_code == status.HTTP_200_OK
         body = response.json()
-        assert body ['status'] == 'OK'
+        assert body['status'] == 'OK'
+
 
         
 
